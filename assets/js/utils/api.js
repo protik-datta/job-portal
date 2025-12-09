a43542c816344a943dc95b90ac8653abfc54dad9
Protik Datta <protikdatta135@gmail.com>
2025-12-09 20:12:28 +0600

    done

--- assets/js/utils/api.js
+++ assets/js/utils/api.js
@@ -36,3 +36,60 @@ export const seeJobDetails = (jobId) => {
       });
   });
 };
+
+export const getJobSearch = new Promise((resolve, reject) => {
+  fetch("https://jobicy.com/api/v2/remote-jobs")
+    .then((res) => {
+      if (!res.ok) {
+        reject("Failed to fetch data");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      resolve(data.jobs);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+const industrySlugs = [
+  "admin-support",
+  "business",
+  "copywriting",
+  "design-multimedia",
+  "supporting",
+  "data-science",
+  "admin",
+  "education",
+  "accounting-finance",
+  "healthcare",
+  "hr",
+  "legal",
+  "marketing",
+  "management",
+  "dev",
+  "seller",
+  "seo",
+  "smm",
+  "engineering",
+  "technical-support",
+  "web-app-design",
+];
+
+export function loadAllIndustryJob() {
+  return Promise.all(
+    industrySlugs.map((slug) => {
+      const url = `https://jobicy.com/api/v2/remote-jobs?industry=${slug}`;
+
+      return fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          return {
+            total: data.jobs?.length || 0,
+            jobs: data.jobs || [],
+          };
+        });
+    })
+  );
+}
